ee404789bd0d050f082d582de1536eccabbc6c68
aaramdhan <arisramdhan13@gmail.com>
2017-07-11 19:06:44 +0700

    edit-aris

---
+++ cekdrop.php
@@ -0,0 +1,65 @@
+<head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link href="css/style.css" rel="stylesheet">
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <nav class="navbar-default" role="navigation">
+    <div class="container">
+
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <a class="navbar-brand" href="index.php">JuraganKos</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Dropdown
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+</body>
+
+<!--
+<head>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Dropdowns</h2>
+  <p>The .dropdown class is used to indicate a dropdown menu.</p>
+  <p>Use the .dropdown-menu class to actually build the dropdown menu.</p>
+  <p>To open the dropdown menu, use a button or a link with a class of .dropdown-toggle and data-toggle="dropdown".</p>                                          
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">HTML</a></li>
+      <li><a href="#">CSS</a></li>
+      <li><a href="#">JavaScript</a></li>
+    </ul>
+  </div>
+</div>
+
+</body>
--- header.php
+++ header.php
@@ -6,16 +6,24 @@
 ?>
 
 	<title>Juragan Kos</title>
-	<!-- CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
+
+  <!--CSS-->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link href="css/style.css" rel="stylesheet">
+
+  <!--JS-->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
 </head>
+
 <body>
 
+   	<!-- Jika tidak login -->
 	<?php
         if(empty($_SESSION['member_username'])) {
     ?>
-   	<!-- NavBar Sebagai Guest-->
+
 	<nav class="navbar-default" role="navigation">
 		<div class="container">
 			<div class="navbar-header">
@@ -38,40 +46,64 @@
 			</div>
 		</div>
 	</nav>
+
+	<!-- Jika Member Login -->
 	<?php
-		} else if(!empty($_SESSION['member_username']))
-            {
+		}else 
+		if(!empty($_SESSION['member_username']))
+           {
             	$member_username = $_SESSION['member_username'];
                 $query_validasi = mysqli_query($link,"SELECT * FROM member WHERE member_username = '$member_username'");
                 $fetch = mysqli_fetch_assoc($query_validasi);
                 extract($fetch);
                 $member_nama = $fetch['member_username'];
     ?>
-    	<nav class="navbar-default" role="navigation">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
+
+    <nav class="navbar-default" role="navigation">
+		<div class="container">
+
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
 				<a class="navbar-brand" href="index.php">JuraganKos</a>
-				</div>
-			<div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+			</div>
+
+			<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">				
-					<li class="menuItem"><a href="">List Kosan</a></li>
-					<ul class="nav navbar-nav navbar-right">
-                		<li class="dropdown">
-	                    	<a href="logout.php"">
-	                    		<i class="fa fa-user-times fa-fw"></i>
-	                    		<?php echo ucwords($member_username); ?>
-	                    		<span class="glyphicon glyphicon-user"></span>
-	                    	</a>
-                		</li>
-                	</ul>
+					<li class="menuItem"><a href="">List Kosan</a></li>	            
+                	<li class="dropdown">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    		<i class="fa fa-user-times fa-fw"></i>
+	                  		<?php echo ucwords($member_username); ?>
+	                  		<span class="glyphicon glyphicon-user"></span>
+                    	</a>
+               			<ul class="dropdown-menu">
+                			<li>
+                    	    	<a href="logout.php">
+                    			<i class="fa fa-sign-out fa-fw"></i>
+                        		Logout
+                        		</a>
+                   			</li>
+                    		<li>
+                        		<a href="">
+                        		<i class="fa fa-pencil fa-fw"></i>
+                        		???
+                        		</a>
+                    		</li>
+                		</ul>
+                	</li>
                 </ul>	
 			</div>
-		</nav>
-	<?php }
+
+		</div>
+	</nav>
+
+	<?php
+	 }
 	?>
+
